61b3be71f374a0638a27e2d72cb66e0fe9059207
Cauê Garcia <cauerg7@outlook.com>
2024-12-22 17:50:57 -0300

    Header component ok

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,10 @@
+import './styles.css'
 
 export default function Header() {
-    
+
+    return (
+        <header>
+            <h1 className='container'>Github API</h1>
+        </header>
+    )
 }
